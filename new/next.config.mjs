207aa24431c775201b10ecefa55a199d4b072f0b
Christian Glassiognon <christian@heyglassy.co>
2022-08-04 00:32:47 -0700

    silence env warnings

--- new/next.config.mjs
+++ new/next.config.mjs
@@ -1,19 +1,19 @@
-// @ts-check
-import { env } from "./src/env/server.mjs";
+// // @ts-check
+// import { env } from "./src/env/server.mjs";
 
-/**
- * Don't be scared of the generics here.
- * All they do is to give us autocompletion when using this.
- *
- * @template {import('next').NextConfig} T
- * @param {T} config - A generic parameter that flows through to the return type
- * @constraint {{import('next').NextConfig}}
- */
+// /**
+//  * Don't be scared of the generics here.
+//  * All they do is to give us autocompletion when using this.
+//  *
+//  * @template {import('next').NextConfig} T
+//  * @param {T} config - A generic parameter that flows through to the return type
+//  * @constraint {{import('next').NextConfig}}
+//  */
 function defineNextConfig(config) {
-  return config;
+    return config;
 }
 
 export default defineNextConfig({
-  reactStrictMode: true,
-  swcMinify: true,
+    reactStrictMode: true,
+    swcMinify: true,
 });
