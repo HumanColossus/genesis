5de1d67e031dee59874a699902724e9563d8c27c
Will DePue <will@depue.net>
2022-08-05 22:11:02 -0700

    styling changes

--- new/src/components/SEO/marquee-layout.tsx
+++ new/src/components/SEO/marquee-layout.tsx
@@ -8,34 +8,24 @@ type Props = {
 export const MarqueeLayout: React.FC<Props> = ({ children }) => {
   return (
     <>
-      <Marquee
-        style={{ backgroundColor: "#5F4BDD" }}
-        gradient={true}
-        gradientColor={[0, 0, 28]}
-        speed={10}
-      >
+      <Marquee style={{ backgroundColor: "#30267C", fontFamily: "mono", fontSize: "16px" }} gradient={true} gradientColor={[0, 0, 28]} speed={10}>
         {true &&
           Array(10)
             .fill(0)
             .map((_, i) => (
-              <p className="m-1.5 mr-12 text-white" key={i}>
-                CONFIDENTIAL DO NOT SHARE
+              <p className="m-1 mr-12 text-white" key={i}>
+                ANNOUNCEMENT SOON
               </p>
             ))}
       </Marquee>
       {children}
-      <Marquee
-        style={{ backgroundColor: "#5F4BDD" }}
-        gradient={true}
-        gradientColor={[0, 0, 28]}
-        speed={10}
-      >
+      <Marquee style={{ backgroundColor: "#5f4bdd", fontFamily: "mono", fontSize: "16px" }} gradient={true} gradientColor={[0, 0, 28]} speed={10}>
         {true &&
           Array(10)
             .fill(0)
             .map((_, i) => (
-              <p className="m-1.5 mr-12 text-white" key={i}>
-                CONFIDENTIAL DO NOT SHARE
+              <p className="m-1 mr-12 text-white" key={i}>
+                ANNOUNCEMENT SOON
               </p>
             ))}
       </Marquee>
