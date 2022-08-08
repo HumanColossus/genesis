6eb6e5c650da65685bee5748262c4ccc3e005e6a
Christian Glassiognon <christian@heyglassy.co>
2022-08-07 21:52:14 -0700

    plausible test 3

--- new/next.config.mjs
+++ new/next.config.mjs
@@ -3,27 +3,41 @@
 
 import { withPlausibleProxy } from "next-plausible";
 
-// /**
-//  * Don't be scared of the generics here.
-//  * All they do is to give us autocompletion when using this.
-//  *
-//  * @template {import('next').NextConfig} T
-//  * @param {T} config - A generic parameter that flows through to the return type
-//  * @constraint {{import('next').NextConfig}}
-//  */
+/**
+ * Don't be scared of the generics here.
+ * All they do is to give us autocompletion when using this.
+ *
+ * @template {import('next').NextConfig} T
+ * @param {T} config - A generic parameter that flows through to the return type
+ * @constraint {{import('next').NextConfig}}
+ */
 
 function defineNextConfig(config) {
-    return withPlausibleProxy()({ config });
+    return { config };
 }
 
-export default defineNextConfig({
-    reactStrictMode: true,
-    swcMinify: true,
-    async redirects() {
-        return [{
-            source: "/",
-            destination: "/memo",
-            permanent: false,
-        }, ];
-    },
-});
+// export default defineNextConfig({
+//     reactStrictMode: true,
+//     swcMinify: true,
+//     async redirects() {
+//         return [{
+//             source: "/",
+//             destination: "/memo",
+//             permanent: false,
+//         }, ];
+//     },
+// });
+
+export default withPlausibleProxy()(
+    defineNextConfig({
+        reactStrictMode: true,
+        swcMinify: true,
+        async redirects() {
+            return [{
+                source: "/",
+                destination: "/memo",
+                permanent: false,
+            }, ];
+        },
+    })
+);
